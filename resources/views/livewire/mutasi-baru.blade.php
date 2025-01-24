c6cddd65a3c72a51d7fb264a04a1d721d91b26b2
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-01-24 09:14:15 +0700

    cek

--- resources/views/livewire/mutasi-baru.blade.php
+++ resources/views/livewire/mutasi-baru.blade.php
@@ -7,7 +7,7 @@
   @else
     <div>
       <h3 class="border border-gray-300 rounded-lg shadow-sm p-4">Terima kasih telah mengisi formulir.</h3>
-      <button wire:click="showTY" class="w-full bg-blue-500 text-white rounded-md mt-3 py-3 px-3">Isi Formulir Kembali</button>
+      
     </div>  
   @endif
   </div>
