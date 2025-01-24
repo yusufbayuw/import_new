2636b9dde7ce51229fdfbd04290caefc43015cea
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-01-24 09:51:12 +0700

    cek relasi kolom

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pegawai extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function mutasi(): HasMany
+    {
+        return $this->hasMany(MutasiPesertaBaru::class, 'no_peg');
+    }
 }
